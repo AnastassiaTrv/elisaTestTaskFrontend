058d1f4b87c2afaed849c67d1af89a3cc21c3d71
atretjakova <anastassia.sp@gmail.com>
2018-05-18 20:29:57 +0300

    shopping cart component replaced with cart info component

---
+++ src/app/components/header/cart-info/cart-info.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import {Observable} from 'rxjs/Observable';
+import {Store} from '@ngrx/store';
+import {State} from '../../../store/app.state';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-cart-info',
+  templateUrl: './cart-info.component.html',
+  styleUrls: ['./cart-info.component.css']
+})
+export class CartInfoComponent implements OnInit {
+
+  constructor(private store: Store<State>, private router: Router) {}
+
+  cartItemsCount$: Observable<number>; // ngrx variable
+
+  ngOnInit() {
+    this.cartItemsCount$ = this.store.select( state => state.shoppingCartData.productsTotal);
+  }
+
+
+  /**
+   * Navigate to shopping cart view
+   */
+  openShoppingCart() {
+    this.router.navigate(['/shoppingCart']);
+  }
+
+}
